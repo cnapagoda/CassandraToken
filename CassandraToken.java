f54437cc3f6f630868e7e4a01d5b469c98d195c0
cnapagoda <cnapagoda@gmail.com>
2013-08-19 22:58:39 +0530

    Adding Java based Token gen

---
+++ CassandraToken.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+public class CassandraToken{
+
+	public static void main(String[] args){
+
+		if (args.length == 0){
+			System.exit(0);
+		}
+
+		int numberOfNode = 3;
+		BigInteger num  = BigInteger.valueOf(numberOfNode);
+		BigInteger power = BigInteger.valueOf(2).pow(127);
+		System.out.println("\n\n");
+		for( int i =0; i < numberOfNode; i++){
+
+			BigInteger temp = power.divide(num);
+			temp = temp.multiply(BigInteger.valueOf(i));
+
+			System.out.println("Node : " + i);
+			System.out.println("initial_token : " + temp);
+			System.out.println("");
+		}
+	}
+}
